e34b83b7103f14695090bc69a82f4db787dce6b7
Alexandre Juca <corextechnologies@gmail.com>
2018-07-29 01:45:05 +0100

    Initial commit

---
+++ lib/KambaCheckout.php
@@ -0,0 +1,111 @@
+<?php
+/**
+ * 
+ * Restfull Kamba Checkout API client
+ * 
+ */
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+include "Request.php";
+include "ValidationException.php";
+
+Class KambaCheckout
+{
+	private $api_endpoint;
+	private $auth_headers;
+	private $merchant_id;
+	private $api_key;
+	private $curl;
+
+	public function phpAlert($msg) {
+		echo '<script type="text/javascript">alert("' . $msg . '")</script>';
+	}
+	
+	 function __construct($merchant_id, $api_key, $test_mode)
+	{
+		$this->curl             =   new Request;
+		$this->merchant_id 		=   $merchant_id;
+		$this->api_key	        =   $api_key;
+		phpAlert("Init KambaCHeckout");
+		
+		if($test_mode)
+			$this->api_endpoint  = "https://sandbox.usekamba.com/v1/checkouts";
+		else
+			$this->api_endpoint  = "https://api.usekamba.com/v1/checkouts";
+	}
+	
+	public function createCheckout($data)
+	{
+		$curl = curl_init();
+		curl_setopt_array($curl, array(
+			CURLOPT_URL => $this->api_endpoint ,
+			CURLOPT_RETURNTRANSFER => true,
+			CURLOPT_MAXREDIRS => 10,
+			CURLOPT_TIMEOUT => 30,
+			CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+			CURLOPT_CUSTOMREQUEST => "POST",
+			CURLOPT_POSTFIELDS => $data,
+			CURLOPT_HTTPHEADER => array(
+				"authorization: Token $this->api_key",
+				"content-type: application/json"
+				)
+		));
+
+		$response = curl_exec($curl);
+	
+		
+
+		setCheckout($response->qr_code->string);
+		
+		$err = curl_error($curl);
+
+		curl_close($curl);
+
+		if ($err) {
+		echo "cURL Error #:" . $err;
+		} else {
+			phpAlert("No error");
+		}
+		
+		if(isset($response))
+		{
+			phpAlert("Response is valid");
+			return $response;
+		} else {
+			$errors = array();  
+			phpAlert("errors: $errors");
+			foreach($response as $key => $value)
+			{
+				if(is_array($value))
+					$errors[] = $value[1];
+			}
+			if($errors)
+				throw new ValidationException("Validation Error Occured with following Errors : ",$errors, $response);
+		}
+	}
+	
+	
+	public function getCheckoutById($id)
+	{	phpAlert("GetCheckoutById");
+		$endpoint   =   $this->api_endpoint."checkouts/$id";
+		$result     =   $this->curl->get($endpoint,array("headers"=>$this->auth_headers));
+        $result     =   json_decode($result);
+        
+		if(isset($result->id) and $result->id)
+			return $result;
+		else
+			throw new Exception("Unable to Fetch Payment Request id:'$id' Server Responds ".print_r($result,true));
+	}
+
+	public function getCheckoutStatus($checkout_id, $checkout){
+		phpAlert("getCheckoutStatus");
+		foreach($checkouts as $checkout){
+		    if($checkout->id == $checkout_id){
+			    return $checkout->status;
+		    }
+		}
+	}
+	
+}
---
+++ payment_gateway.php
@@ -0,0 +1,151 @@
+<?php
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+function phpAlert($msg) {
+    echo '<script type="text/javascript">alert("' . $msg . '")</script>';
+}
+
+Class WP_Gateway_Kamba extends WC_Payment_Gateway{
+	
+	private $testmode;
+	private $merchant_id;
+	private $api_key;
+	
+	
+	public function __construct()
+	{
+		$this->id                   = "kamba-woocommerce";
+		$this->icon                 = "https://avatars0.githubusercontent.com/u/29748778?s=200&v=4";
+		$this->has_fields           = false;
+		$this->method_title         = "Kamba Checkout";
+		$this->method_description   = "Recargas de saldo e pagamentos na Internet com dinheiro";
+	
+		$this->init_form_fields();
+		$this->init_settings();
+		
+		$this->title            = $this->get_option( 'title' );
+		$this->description      = $this->get_option( 'description' );
+		$this->testmode         = 'yes' === $this->get_option( 'testmode', 'no' );
+		$this->merchant_id      = $this->get_option( 'client_id' );
+		$this->api_key          = $this->get_option( 'client_secret' );
+		add_action( 'woocommerce_update_options_payment_gateways_' . $this->id, array( $this, 'process_admin_options' ) );
+	}
+	
+	public function init_form_fields()
+	{
+		$this->form_fields = include("kamba-settings.php");		
+	}
+
+	
+	public function process_payment($orderId)
+	{
+		include_once "lib/KambaCheckout.php";
+		phpAlert("Creating Kamba Checkout Order for order id: $orderId");
+		phpAlert("Merchant ID: $this->merchant_id | API Key: $this->api_key  | Testmode: $this->testmode ");
+		
+		$order = new WC_Order( $orderId );
+		// Get an instance of the WC_Order object
+		$product_names = array();
+
+		// Iterating through each WC_Order_Item_Product objects
+		foreach ($order->get_items() as $item_key => $item_values) {
+			// Item ID is directly accessible from the $item_key in the foreach loop or
+			$item_id = $item_values->get_id();
+			## Using WC_Order_Item_Product methods ##
+			$item_name = $item_values->get_name(); // Name of the product
+			array_push($product_names, $item_name);
+			$item_type = $item_values->get_type(); // Type of the order item ("line_item")
+
+			$product_id = $item_values->get_product_id(); // the Product id
+			$product = $item_values->get_product(); // the WC_Product object
+			## Access Order Items data properties (in an array of values) ##
+			$item_data = $item_values->get_data();
+
+			$product_name = $item_data['name'];
+			$product_id = $item_data['product_id'];
+		
+		}
+
+	
+        
+		try {
+			
+			$request = new KambaCheckout($this->merchant_id, $this->api_key, $this->testmode);
+            
+            $request_data['channel']                = "WEBSITE";
+            $request_data['currency'] 		        = "AOA";
+			$request_data['initial_amount'] 	    = $this->get_order_total();
+			$request_data['notes'] 		            = "Oooo oo";
+			$request_data['redirect_url_success'] 	= "http://localhost:8888/kamba_test/checkout/paywithkamba/";
+            $request_data['payment_method']         = "WALLET";
+            
+			#$this->debug("Data sent for creating order ".print_r($request_data, true));
+			$response = $request->createCheckout(json_encode($request_data));
+            
+			if(isset($response))
+			{	phpAlert("is set!");
+				$response = json_decode($response);
+				$url = $response->redirect_url_success;
+				WC()->session->set( 'payment_request_id',  $response->id);	
+				phpAlert($url);
+				die(0);
+				return array(
+                    'result'    => 'success', 
+                    'redirect'  => $url
+				);
+			}
+		
+		} catch(CurlException $e) {
+			phpAlert("An error occurred on line " . $e->getLine() . " with message " .  $e->getMessage());
+			phpAlert("Traceback: " . (string)$e);
+			$json = array(
+				"result"    =>  "failure",
+				"messages"  =>  "<ul class=\"woocommerce-error\">\n\t\t\t<li>" . $e->getMessage() . "</li>\n\t</ul>\n",
+				"refresh"   =>  "false",
+				"reload"    =>  "false"
+				);
+			phpAlert($json);
+			die(json_encode($json));
+		} catch(ValidationException $e) {
+			phpAlert("Validation Exception Occured with response ".print_r($e->getResponse(), true));
+			$errors_html = "<ul class=\"woocommerce-error\">\n\t\t\t";
+			foreach( $e->getErrors() as $error)
+			{
+				$errors_html .="<li>".$error."</li>";
+				
+			}
+			$errors_html .= "</ul>";
+			$json = array(
+				"result"    =>  "failure",
+				"messages"  =>  $errors_html,
+				"refresh"   =>  "false",
+				"reload"    =>  "false"
+				);
+				phpAlert($json);
+			die(json_encode($json));
+		}
+		catch(Exception $e){
+			
+			phpAlert("An error occurred on line " . $e->getLine() . " with message " .  $e->getMessage());
+			phpAlert("Traceback: " . $e->getTraceAsString());
+			$json = array(
+				"result"    =>  "failure",
+				"messages"  =>  "<ul class=\"woocommerce-error\">\n\t\t\t<li>".$e->getMessage()."</li>\n\t</ul>\n",
+				"refresh"   =>  "false",
+				"reload"    =>  "false"
+				);
+			phpAlert($json);
+			die(json_encode($json));
+			
+		}
+	}
+	
+	public static function log( $message ) 
+	{
+		kamba_log($message);
+	}
+	
+}
